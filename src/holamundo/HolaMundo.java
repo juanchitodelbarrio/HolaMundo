9e3108d2e504d4741d18d56ec74e682d37a4f34c
juandavidduranbermudez <juandebermudez14@gmail.com>
2018-02-27 14:31:49 -0500

    se agraga clase pruebagit

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -3,6 +3,13 @@ package holamundo;
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.print("HOLA MUNDO DESDE GIT");
+		System.out.print("PRUEBAS CON RAMAS");
 	}
 
+}
+
+class PruebaGit{
+	public PruebaGit() {
+		// TODO Auto-generated constructor stub
+	}
 }
