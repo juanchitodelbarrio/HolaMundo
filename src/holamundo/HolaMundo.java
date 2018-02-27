bace63f7742fb14909f8219be61b414c809b336b
juandavidduranbermudez <juandebermudez14@gmail.com>
2018-02-27 13:49:36 -0500

    SE HIZO CAMBIO EN EL SYSTEM

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -2,7 +2,7 @@ package holamundo;
 
 public class HolaMundo {
 	public static void main(String[] args) {
-		System.out.print("HOLA MUNDO DESDE UBUNTU =)");
+		System.out.print("HOLA MUNDO DESDE GIT");
 	}
 
 }
