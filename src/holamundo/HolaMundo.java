dce938634e8b7a72873c98fcec792b41c8e3e3b8
juandavidduranbermudez <juandebermudez14@gmail.com>
2018-02-27 15:20:20 -0500

    agregando metodos

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -5,11 +5,25 @@ public class HolaMundo {
 		System.out.print("HOLA MUNDO DESDE GIT");
 		System.out.print("PRUEBA RAMA MASTER");
 		System.out.print("PRUEBAS CON RAMAS");
+		
+		PruebaGit pruebaGit=new PruebaGit();
+		System.out.println(pruebaGit.getCampoDeClase1());
 	}
 }
 
 class PruebaGit{
 	public PruebaGit() {
 		// TODO Auto-generated constructor stub
+		campoDeClase1="ESTE ES UN CAMPO DE CLASE";
 	}
+	
+	public String getCampoDeClase1() {
+		return campoDeClase1;
+	}
+
+	public void setCampoDeClase1(String campoDeClase1) {
+		this.campoDeClase1 = campoDeClase1;
+	}
+
+	private String campoDeClase1;
 }
